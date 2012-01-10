27489500dc7521f814a59f2b7e415bd8ecb5751d
Simon Riley <syriley@gmail.com>
2012-01-10 19:33:19 +0000

    update tab tweaked to have no order

--- app/models/Tab.java
+++ app/models/Tab.java
@@ -56,11 +56,10 @@ public class Tab extends Model {
 			Datastore datastore = Tab.ds();
 			MorphiaQuery morphiaQuery = Tab.q();
 			morphiaQuery.filter("type", tabType);
-			morphiaQuery.filter("dateTabStarted = ", null);
-			morphiaQuery.filter("dateTabCompleted = ", null);
+			morphiaQuery.filter("dateTabStarted", null);
+			morphiaQuery.filter("dateTabCompleted", null);
 			//query.or(query.criteria("expire").equal("false"), query.criteria("expire").notEqual(true));
 			//morphiaQuery.filter("expire != ", false);
-			morphiaQuery.order("-numberOfVotes");
 			Query query = morphiaQuery.limit(1).getMorphiaQuery();
 			UpdateOperations<Tab> updateOperations = datastore.createUpdateOperations(Tab.class).set("dateTabStarted", new Date());
 			tab = datastore.findAndModify(query, updateOperations);
