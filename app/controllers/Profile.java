25b9d9f0351faceabb7cc2879acfb952cad3b96f
Simon Riley <syriley@gmail.com>
2012-02-10 07:53:09 +0000

    profile added

---
+++ app/controllers/Profile.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Profile extends Controller {
+	public static void index() {
+		 render();
+	 }
+	
+	public static void edit() {
+		 render();
+	 }
+}
