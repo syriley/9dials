7484b4a0cca6bb6461ff9d72fa2b2cce8b9d61ee
Simon Riley <syriley@gmail.com>
2012-02-19 16:26:34 +0000

    morphia controllers and models commented out

--- app/controllers/Tabs.java
+++ app/controllers/Tabs.java
@@ -5,48 +5,48 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import models.Tab;
+//import models.Tab;
 import play.mvc.Controller;
 
 import com.google.gson.Gson;
 
 public class Tabs extends Controller {
         
-    public static void index(String type) {
-        Tab tab = Tab.getOne("Tabs", type);
-        render(tab);
-    }
-    
-    public static void show(String id) {
-        Tab tab = Tab.findById(id);
-        render(tab);
-    }
-    
-    public static void next(String type) {
-        Tab tab = Tab.getOne("Chords", type);
-        renderJSON(tab);
-    }
-    
-    public static void create () {
-        Gson gson = new Gson();
-        String data = params.get("data");
-        Tab tab = gson.fromJson(data, Tab.class);
-        tab.save();
-    }
-    
-
-    public static void list() {
-        String search = params.get("search");
-        List<Tab> tabs = Tab.list(search);
-        List<Map<String,String>> result = new ArrayList<Map<String,String>>();
-        for (Tab tab : tabs) {
-            Map<String,String> resultItem = new HashMap<String, String>();
-            resultItem.put("idString", tab.getId().toString());
-            resultItem.put("artist", tab.artist);
-            resultItem.put("title", tab.title);
-            result.add(resultItem);
-    }
-	renderJSON(result);
-	}
+//    public static void index(String type) {
+//        Tab tab = Tab.getOne("Tabs", type);
+//        render(tab);
+//    }
+//    
+//    public static void show(String id) {
+//        Tab tab = Tab.findById(id);
+//        render(tab);
+//    }
+//    
+//    public static void next(String type) {
+//        Tab tab = Tab.getOne("Chords", type);
+//        renderJSON(tab);
+//    }
+//    
+//    public static void create () {
+//        Gson gson = new Gson();
+//        String data = params.get("data");
+//        Tab tab = gson.fromJson(data, Tab.class);
+//        tab.save();
+//    }
+//    
+//
+//    public static void list() {
+//        String search = params.get("search");
+//        List<Tab> tabs = Tab.list(search);
+//        List<Map<String,String>> result = new ArrayList<Map<String,String>>();
+//        for (Tab tab : tabs) {
+//            Map<String,String> resultItem = new HashMap<String, String>();
+//            resultItem.put("idString", tab.getId().toString());
+//            resultItem.put("artist", tab.artist);
+//            resultItem.put("title", tab.title);
+//            result.add(resultItem);
+//    }
+//	renderJSON(result);
+//	}
 
 }
--- app/models/Band.java
+++ app/models/Band.java
@@ -20,50 +20,50 @@ import com.google.code.morphia.query.UpdateOperations;
 @Entity 
 public class Band extends Model {
         
-        Logger logger = LoggerFactory.getLogger(Band.class);
-        private static final long serialVersionUID = 6397424855980948476L;
-        public String idString;
-        public String name;
-        public Date dateCompleted;
-        public Date dateStarted;
-        public String url;      
-        public int numberOfTabs;
-        
-        public URL getUrl() {
-                try {
-                        return new URL (url);
-                } catch (MalformedURLException e) {
-                        logger.error(e.toString());
-                }
-                return null;
-        }
-        public void setUrl(URL url) {
-                this.url = url.toString();
-        }
-        public static Band getNext() {
-            Query query = Band.q()
-                            .filter("dateCompleted = ", null)       
-                            .filter("dateStarted != ", null)
-                            .order("-numberOfTabs")
-                            .limit(1).getMorphiaQuery();
-            Datastore datastore = Band.ds();
-            UpdateOperations<Band> updateOperations = datastore.createUpdateOperations(Band.class).set("dateStarted", new Date());
-            Band band = datastore.findAndModify(query,updateOperations);
-            return band;
-    }
-    
-    public static List<Band> list(String search) {
-            
-            MorphiaQuery query = Band.q();
-            if (StringUtils.isNotEmpty(search)) {
-                    query.filter("name", Pattern.compile(search, Pattern.CASE_INSENSITIVE));
-            }
-            return query.limit(20).asList();
-    }
-    public static List<Band> search(String searchTerm) {
-            List<Band> bands = Band.q().filter("name",
-                            Pattern.compile(searchTerm, Pattern.CASE_INSENSITIVE)).asList();
-            
-            return bands;
-    }
+//        Logger logger = LoggerFactory.getLogger(Band.class);
+//        private static final long serialVersionUID = 6397424855980948476L;
+//        public String idString;
+//        public String name;
+//        public Date dateCompleted;
+//        public Date dateStarted;
+//        public String url;      
+//        public int numberOfTabs;
+//        
+//        public URL getUrl() {
+//                try {
+//                        return new URL (url);
+//                } catch (MalformedURLException e) {
+//                        logger.error(e.toString());
+//                }
+//                return null;
+//        }
+//        public void setUrl(URL url) {
+//                this.url = url.toString();
+//        }
+//        public static Band getNext() {
+//            Query query = Band.q()
+//                            .filter("dateCompleted = ", null)       
+//                            .filter("dateStarted != ", null)
+//                            .order("-numberOfTabs")
+//                            .limit(1).getMorphiaQuery();
+//            Datastore datastore = Band.ds();
+//            UpdateOperations<Band> updateOperations = datastore.createUpdateOperations(Band.class).set("dateStarted", new Date());
+//            Band band = datastore.findAndModify(query,updateOperations);
+//            return band;
+//    }
+//    
+//    public static List<Band> list(String search) {
+//            
+//            MorphiaQuery query = Band.q();
+//            if (StringUtils.isNotEmpty(search)) {
+//                    query.filter("name", Pattern.compile(search, Pattern.CASE_INSENSITIVE));
+//            }
+//            return query.limit(20).asList();
+//    }
+//    public static List<Band> search(String searchTerm) {
+//            List<Band> bands = Band.q().filter("name",
+//                            Pattern.compile(searchTerm, Pattern.CASE_INSENSITIVE)).asList();
+//            
+//            return bands;
+//    }
 }
