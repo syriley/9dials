2780c03ded6f42b6868a7de369302ce2e47168fa
Tim Williams <timwemail@gmail.com>
2012-02-20 21:16:01 +0000

    refactor auth so we can access user on front page

---
+++ app/controllers/UserCheckController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import models.User;
+import play.Logger;
+import play.mvc.Before;
+import play.mvc.Controller;
+import play.mvc.With;
+import securesocial.provider.SocialUser;
+import controllers.securesocial.SecureSocial;
+import controllers.securesocial.SecureSocialPublic;
+
+@With( SecureSocialPublic.class )
+public class UserCheckController extends Controller {
+	private static final String GET = "GET";
+	private static final String ROOT = "/";
+	private static final String ORIGINAL_URL = "originalUrl";
+
+	private static void rememberOriginalUrl(){
+		final String originalUrl = request.method.equals(GET) ? request.url : ROOT;
+        flash.put(ORIGINAL_URL, originalUrl);
+	}
+	
+	@Before
+    static void setConnectedUser() {    	
+		rememberOriginalUrl();
+		SocialUser user =  SecureSocial.getCurrentUser();
+		if(user!=null && !session.contains("_user")){
+			User ouruser = User.find("byEmail", user.email).first();
+			if(ouruser!=null){
+			   renderArgs.put("_user", ouruser);
+			}
+		}
+    }
+}
