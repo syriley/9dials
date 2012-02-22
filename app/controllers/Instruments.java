05b27bbfd85514b41557717b5caa2950dabb31a1
Tim Williams <timwemail@gmail.com>
2012-02-22 11:47:55 +0000

    Add autocomplete for Instrument adding

--- app/controllers/Instruments.java
+++ app/controllers/Instruments.java
@@ -1,20 +1,47 @@
 package controllers;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import play.Logger;
 
 import models.Instrument;
 
 public class Instruments extends LoggedInController {
+	public static final int MAX_ITEMS = 10;
+	
+	public static void list(String term){
+		List<String> instrumentNames = filterInstruments(term);
+		renderJSON(instrumentNames);
+	}
 
-	public static void list(){
-		List<Instrument> allInstruments = Instrument.findAll();
+	public static List<String> filterInstruments(String term){
+		List<Instrument> matchedInstruments = findMatchingInstruments(term);		
+		List<String> instrumentNames = extractInstrumentNames(matchedInstruments);
+		return instrumentNames;
+	}			
+	
+	private static List<Instrument> findMatchingInstruments(String term) {
+		List<Instrument> allInstruments = Collections.emptyList();
+		if(term==null|| term.equals("")){
+			allInstruments = Instrument.find("order by name").fetch(MAX_ITEMS);
+		}
+		else{
+			allInstruments = Instrument.find("name like ? order by name","%"+term+"%").fetch(MAX_ITEMS);	
+		}
+		return allInstruments;
+	}
+
+	private static List<String> extractInstrumentNames(
+			List<Instrument> allInstruments) {
+		if(allInstruments.isEmpty()){
+			return Collections.emptyList();
+		}
 		List<String> instrumentNames = new ArrayList<String>();
 		for(Instrument i : allInstruments){
 			instrumentNames.add(i.name);
 		}
-		renderJSON(instrumentNames);
+		return instrumentNames;
 	}
-	
-	
 }
---
+++ test/com/ninedials/test/InstrumentsTest.java
@@ -0,0 +1,19 @@
+package com.ninedials.test;
+
+import java.util.List;
+
+import models.OAuthUserHelper;
+import models.User;
+
+import org.junit.Test;
+
+import controllers.Instruments;
+
+public class InstrumentsTest extends NineDialsTest{
+
+	 @Test
+	    public void testBlankSearch() {
+	    	List<String> instruments = Instruments.filterInstruments(null);
+	    	assertEquals("Blank search expected to match only "+Instruments.MAX_ITEMS+" items",Instruments.MAX_ITEMS, instruments.size());
+	    }
+}
