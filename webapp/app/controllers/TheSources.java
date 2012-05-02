e06b5e6b3bb46a19f22bb980d959d061a2b8db0f
Simon Riley <syriley@gmail.com>
2012-05-02 20:23:27 +0100

    The sources added, facebook probably wants to be refactored out

---
+++ webapp/app/controllers/TheSources.java
@@ -0,0 +1,82 @@
+package controllers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import models.Source;
+import models.User;
+
+import org.apache.commons.io.IOUtils;
+
+import play.Logger;
+import play.modules.facebook.FbGraphException;
+import play.mvc.Router;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+public class TheSources extends LoggedInController{
+	  
+	public static final String AWS_ACCESS_KEY = "AKIAJLGV564C5LYG5WHQ";
+	public static final String AWS_SECRET_KEY = "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";
+    public static final String BUCKET_NAME = "media.9dials.com";
+  
+    public static String getFileUrl(String fileName){
+    	return "http://"+BUCKET_NAME+"/"+fileName;
+    }
+    
+	public static void index() {
+		render();
+	}
+
+	public static void audio() {
+		render();
+	}
+	
+	public static void upload(File file, String name) throws FileNotFoundException, FbGraphException {
+		User user = null;	
+	    AWSCredentials awsCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY);
+	    AmazonS3 s3Client = new AmazonS3Client(awsCredentials);
+	    String s3key = UUID.randomUUID().toString();
+	    s3Client.putObject(BUCKET_NAME, s3key, file);
+	    String url = getLocalUrl(s3key);
+	    Source source = new Source(user,file.getName(),s3key,name, url);
+	    source.save();
+	    renderJSON(source);
+	}
+	
+	private static String getLocalUrl(String s3key) {
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("sourceId", s3key);	
+		String url = Router.getFullUrl("MyClips.index", map);
+		return url;
+	}
+
+	private static String getPlayableUrl(String s3key) {
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("sourceId", s3key);	
+		String url = Router.getFullUrl("Share.index", map);
+		return url;
+	}
+	
+	public static void uploadLocally(File file) {
+	    
+	    FileOutputStream moveTo = null;
+	    try {
+	        moveTo = new FileOutputStream(new File("/tmp/test.ogg"));
+	        FileInputStream fileInputStream = new FileInputStream(file);
+            IOUtils.copy(fileInputStream, moveTo);
+        } catch (IOException e) {
+            Logger.error(e, "local upload error");
+        }
+	}
+}
