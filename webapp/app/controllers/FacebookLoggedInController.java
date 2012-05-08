99ebc534968ff4a65c203066380181a6c81bc8bb
Tim Williams <timwemail@gmail.com>
2012-05-08 23:33:17 +0100

    Fix typo in error message

--- webapp/app/controllers/FacebookLoggedInController.java
+++ webapp/app/controllers/FacebookLoggedInController.java
@@ -19,7 +19,7 @@ public class FacebookLoggedInController extends Controller{
 	            }
 	            renderArgs.put("fbuser", fbUser); // put the email into the session (for the Secure module)
 	        } catch (FbGraphException fbge) {
-	            flash.error("Sorry, we can't find you Facebook account. Please try logging in again");
+	            flash.error("Sorry, we can't find your Facebook account. Please try logging in again");
 	            if (fbge.getType() != null && fbge.getType().equals("OAuthException")) {
 	                Session.current().remove("fbuserid");
 	            }
