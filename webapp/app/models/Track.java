d267f7dec78f4ab23d192368dbbab928f247f290
Simon Riley <syriley@gmail.com>
2012-06-02 08:47:34 +0100

    class fix for track

--- webapp/app/models/Track.java
+++ webapp/app/models/Track.java
@@ -11,15 +11,15 @@ import play.db.jpa.Model;
 @Entity
 public class Track extends Model {
 
-    public static JsonObject create(String body, sessionId) {
-/*        Session seshion = Session.findById(sessionId);
+    public static JsonObject create(String body, Long sessionId) {
+        Session seshion = Session.findById(sessionId);
         JsonObject jsonTrack = JsonUtils.getJsonEObject(body);
         JsonObject root = JsonUtils.getJsonEObject(seshion.data);
         Version version = new Version().save();
         jsonTrack.addProperty("id", version.id);
-        jsonObject mergedSession = JsonUtils.mergeJsonObjects(root, jsonTrack, "tracks");
+        JsonObject mergedSession = JsonUtils.mergeJsonObjects(root, jsonTrack, "tracks");
         seshion.data = mergedSession.toString();
         seshion.save();
-        return mergedSession;*/
+        return mergedSession;
     }
 }
